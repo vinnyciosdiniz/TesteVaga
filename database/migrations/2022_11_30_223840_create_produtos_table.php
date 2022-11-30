2650609e37a1c5ad8f43f7fe8e834e41b83bd81a
Vinnycios Diniz <vinny07maria29@gmail.com>
2022-11-30 19:46:11 -0300

    :sparkles: feat: Migrations

--- database/migrations/2022_11_30_223840_create_produtos_table.php
+++ database/migrations/2022_11_30_223840_create_produtos_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
     Schema::create('produtos', function (Blueprint $table) {
+        
         $table->id();
         $table->string('nome');
         $table->string('valor');
